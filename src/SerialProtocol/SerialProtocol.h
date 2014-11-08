90270a501775cf660a6f27b3fa51f13355c379af
Victor Hurdugaci <noreply@victorhurdugaci.com>
2014-11-07 19:56:29 -0800

    Raspberry PI support

--- src/SerialProtocol/SerialProtocol.h
+++ src/SerialProtocol/SerialProtocol.h
@@ -1,8 +1,12 @@
 // Copyright Victor Hurdugaci (http://victorhurdugaci.com). All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
-#include "Arduino.h";
-#include "HardwareSerial.h"
+#ifndef SERIALPROTOCOL_H
+#define SERIALPROTOCOL_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
 
 struct ProtocolState 
 {
@@ -11,30 +15,27 @@ struct ProtocolState
         // The serial object was not set
         NO_SERIAL = 0,
 
+         // The operation succeeded
+        SUCCESS = 1,
+
         // There is not (enought) data to process
-        NO_DATA = 1,
+        NO_DATA = 2,
+        
+         // The object is being received but the buffer doesn't have all the data
+        WAITING_FOR_DATA = 3,
 
         // The size of the received payload doesn't match the expected size
-        INVALID_SIZE = 2,
+        INVALID_SIZE = 4,
 
         // The object was received but it is not the same as one sent
-        INVALID_CHECKSUM = 3,
-
-        // The object is being received but the buffer doesn't have all the data
-        WAITING_FOR_DATA = 4,
-
-        // The operation succeeded
-        SUCCESS = 5
+        INVALID_CHECKSUM = 5
     };
 };
 
 class SerialProtocol
 {
 public:
-    SerialProtocol(HardwareSerial*, uint8_t*, uint8_t);
-
-    // Sets the serial object
-    void setSerial(HardwareSerial*);
+    SerialProtocol(uint8_t*, uint8_t);
 
     // Sends the current payload
     //
@@ -48,12 +49,13 @@ public:
     // Returns a ProtocolState enum value
     uint8_t receive();
 
+protected:
+    virtual bool serialAvailable() = 0;
+
+    virtual void sendData(uint8_t data) = 0;
+    virtual uint8_t readData() = 0;
+
 private:
-    void resetInput();
-    uint8_t canReadMessageBytes();
-
-    HardwareSerial *serial;
-
     uint8_t* payload;
     uint8_t payloadSize;
 
@@ -62,3 +64,5 @@ private:
 
     uint8_t actualChecksum;
 };
+
+#endif
